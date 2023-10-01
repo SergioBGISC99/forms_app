f49a3bb332b281b5f3f80264bc78aaeaf7cc69a6
Sergio Barreras González <sergiobg.isc@gmail.com>
2023-10-01 09:10:14 -0700

    fisrt commit

---
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_cubit/counter_cubit.dart';
+
+class CubitCounterScreen extends StatelessWidget {
+  const CubitCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (_) => CounterCubit(), child: const _CubitCounterView());
+  }
+}
+
+class _CubitCounterView extends StatelessWidget {
+  const _CubitCounterView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cubit Counter'),
+        actions: [
+          IconButton(
+            onPressed: () => {},
+            icon: const Icon(Icons.refresh_rounded),
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text('Counter value xxxx'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+            onPressed: () => {},
+            child: const Text('+3'),
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: '2',
+            onPressed: () => {},
+            child: const Text('+2'),
+          ),
+          const SizedBox(height: 15),
+          FloatingActionButton(
+            heroTag: '3',
+            onPressed: () => {},
+            child: const Text('+1'),
+          )
+        ],
+      ),
+    );
+  }
+}
