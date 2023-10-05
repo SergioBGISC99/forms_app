1b1f397515648f3e47c37176fa3d72cbf6adf42b
Sergio Barreras González <sergiobg.isc@gmail.com>
2023-10-04 17:02:43 -0700

    BLoC implementado

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -14,6 +14,12 @@ class HomeScreen extends StatelessWidget {
             subtitle: const Text('Gestor de estado simple'),
             trailing: const Icon(Icons.arrow_forward_rounded),
             onTap: () => GoRouter.of(context).push('/cubits'),
+          ),
+          ListTile(
+            title: const Text('BLoC'),
+            subtitle: const Text('Gestor de estado compuesto'),
+            trailing: const Icon(Icons.arrow_forward_rounded),
+            onTap: () => GoRouter.of(context).push('/bloc'),
           )
         ],
       ),
